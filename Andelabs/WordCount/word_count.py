657312fa8b288089b58a694c916f20054a9a4c3b
Kenneth Lebu <kenlebu@gmail.com>
2017-04-19 19:53:16 +0300

    modified the function so that integers are counted as integers and not strings. This solved the case of test_word_occurance5 failing

--- Andelabs/WordCount/word_count.py
+++ Andelabs/WordCount/word_count.py
@@ -2,16 +2,21 @@ class WordCount:
     @staticmethod
     def words(sentence):
         dictionary = {}
-        words_list = sentence.split()
+        words_list = sentence.strip("\t\n\r\v\f").split() #spliting at white spaces characters to make a list of the words
+        counter = 0
+        """ Looping through the words, changing the numbers to int objects """
         for each in words_list:
             try:
-                each = int(each)
-            except Exception:
-                pass
-
+                words_list[counter] = int(words_list[counter])
+                counter += 1
+            except ValueError:
+                counter += 1
         
         for word in (words_list):
             if word in dictionary.keys(): dictionary[word] += 1
             else: dictionary[word] = 1
 
         return dictionary
+
+
+print(WordCount.words("Hello 1 2 testing"))
