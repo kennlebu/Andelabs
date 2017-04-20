5d8701277d73517225d30a058abc826909ac2813
Kenneth Lebu <kenlebu@gmail.com>
2017-04-20 10:57:44 +0300

    Added the missing_number function

---
+++ Andelabs/MissingNumber/missing_number.py
@@ -0,0 +1,16 @@
+""" This function takes two lists as input, with one list
+    having an extra entry not contained in the other list.
+    The function returns the missing entry. """
+
+class MissingNumber:
+    @staticmethod
+    def find_missing(list1, list2):
+        if len(list1) == 0 and len(list2) == 0:
+            return 0
+
+        if len(list1) == len(list2):
+            return 0
+
+        for number in list2:
+            if number not in list1:
+                return number
