7eddc65c3f31d434e26b13890d91c74d86f8a9a6
Kenneth Lebu <kenlebu@gmail.com>
2017-04-20 21:13:18 +0300

    Removed the sorting algorithm because the list is constructed in a sorted manner already

--- Andelabs/BinarySearch/binary_search.py
+++ Andelabs/BinarySearch/binary_search.py
@@ -9,7 +9,6 @@ class BinarySearch(list):
             self.array_item += self.step
 
         self.length = len(self)
-        self = self.sort_array(self)
             
 
     def search(self, value):
@@ -26,15 +25,15 @@ class BinarySearch(list):
            found = True
            index = high
 
-       # Return -1 as index if the value doesnt exist in the list
+       # Return index -1 if the value is not on the list
        if value not in self:
            found = True
            index = -1
-           
-       # binary search for the item
+
+       # binary search
        while low <= high and not found:
-           counter += 1 # Register an iteration
-           middle = (low + high) //    # Get the midpoint
+           counter += 1
+           middle = (low + high) // 2    # Get the midpoint
            if self[middle] == value:
                found = True
                index = middle
@@ -45,22 +44,3 @@ class BinarySearch(list):
                    low = middle + 1
                    
        return {'count': counter, 'index': index}
-        
-
-    def sort_array(self, array):
-        """ Sorts and returns the list passed to it """
-
-        for i in range(len(array)-1, 0, -1):
-            maxpos = 0
-            for position in range(1, i+1):
-                if array[position] > array[maxpos]:
-                    maxpos = position
-
-            array[i], array[maxpos] = array[maxpos], array[i]
-
-        return array
-
-b = BinarySearch(20, 2)
-#print(b[16])
-print(b.search(40))
-#print(b)
